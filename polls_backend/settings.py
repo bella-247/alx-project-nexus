3380ddb633c784046e25c5e514b59fb34c5a7f89
Abel Mekonnen <abelmekonen247@gmail.com>
2025-12-02 23:09:35 +0300

    Add STATIC_ROOT setting for production static file collection

--- polls_backend/settings.py
+++ polls_backend/settings.py
@@ -113,6 +113,8 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
+# Directory where `collectstatic` will gather static files for production
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
